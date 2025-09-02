de91fd37e75dc3d1e7e3675c91bf65d539a05ba4
sneshev <steffal.n2@gmail.com>
2025-09-02 12:37:55 +0200

    put print_action() lock (lines were writing into each other before)

--- src/routine/print_action.c
+++ src/routine/print_action.c
@@ -2,6 +2,9 @@
 
 void	print_action(size_t index, e_action act)
 {
+	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+	pthread_mutex_lock(&lock);
 	print_elapsed_ms();
 	printf(" %zu ", index);
 	if (act == FORK)
@@ -15,4 +18,5 @@ void	print_action(size_t index, e_action act)
 	else if (act == THINK)
 		printf("is thinking");
 	printf("\n");
+	pthread_mutex_unlock(&lock);
 }
