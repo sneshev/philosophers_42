0ec3dc99c7f17863b885d6daf0adc8c162a18630
sneshev <steffal.n2@gmail.com>
2025-09-09 21:05:27 +0200

    with thinking seems ok?

--- src/routine/think.c
+++ src/routine/think.c
@@ -10,10 +10,10 @@ void	think(t_philosopher *philo)
 	ms_since_last_meal = get_elapsed_ms() - philo->meal_last.val;
 	print_action(philo->index, THINK);
 	to_think_ms = (ms_to_die - ms_since_last_meal) / 2;
-	printf("(%ld)meal_last      : %ld\n", philo->index, philo->meal_last.val);
-	printf("(%ld)elapsed ms     : %ld\n", philo->index, get_elapsed_ms());
-	printf("(%ld)since last meal: %d\n", philo->index, ms_since_last_meal);
-	printf("(%ld)to_die_ms    : %d\n", philo->index, ms_to_die);
-	printf("(%ld)to_think_ms    : %d\n", philo->index, to_think_ms);
+	// printf("(%ld)meal_last      : %ld\n", philo->index, philo->meal_last.val);
+	// printf("(%ld)elapsed ms     : %ld\n", philo->index, get_elapsed_ms());
+	// printf("(%ld)since last meal: %d\n", philo->index, ms_since_last_meal);
+	// printf("(%ld)to_die_ms    : %d\n", philo->index, ms_to_die);
+	// printf("(%ld)to_think_ms    : %d\n", philo->index, to_think_ms);
 	perform_action(philo, to_think_ms);
 }
