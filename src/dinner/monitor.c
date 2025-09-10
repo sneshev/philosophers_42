d0d5f43c32f738a22db709a602d593be66f2a31d
sneshev <steffal.n2@gmail.com>
2025-09-10 19:02:12 +0200

    working on monitoring function

--- src/dinner/monitor.c
+++ src/dinner/monitor.c
@@ -1,7 +1,33 @@
 #include "../philosophers.h"
 
+bool	has_died(t_philosopher *philo)
+{
+	long	elapsed_ms;
+	long	to_die_ms;
+	bool	result;
+
+	elapsed_ms = get_elapsed_ms();
+	to_die_ms = philo->config.time_to_die;
+	pthread_mutex_lock(&philo->meal_last.lock);
+	if (elapsed_ms - philo->meal_last.val >= to_die_ms)
+		result = true;
+	else
+		result = false;
+	pthread_mutex_unlock(&philo->meal_last.lock);
+	return (result);
+}
+// 400 - 200
+
 void	monitor(t_dinner *dinner, t_philosopher *philos)
 {
-	(void)dinner;
-	(void)philos;
+	int	i;
+
+	i = 0;
+	while (1)
+	{
+		while(i < dinner->config.philos_count)
+		{
+			i++;
+		}
+	}
 }
